e35a6af0c4b4f17734a6f955ff560427439b8d14
Aditya Kumar Tiwari <adityak8340@gmail.com>
2024-06-11 23:08:53 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import os
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # Import load_dotenv to load the .env file
 import time
+
+# Load environment variables from the .env file
+load_dotenv()
+
 from groq import Groq
+import random
+
 from langchain.chains import ConversationChain, LLMChain
 from langchain_core.prompts import (
     ChatPromptTemplate,
@@ -13,10 +19,7 @@ from langchain_core.messages import SystemMessage
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain_groq import ChatGroq
 from langchain.prompts import PromptTemplate
-import speech_recognition as sr
 
-# Load environment variables from the .env file
-load_dotenv()
 
 def display_typing_effect(text, delay):
     """
@@ -32,75 +35,12 @@ def display_typing_effect(text, delay):
         placeholder.markdown(f'<div style="word-wrap: break-word;">{typed_text}</div>', unsafe_allow_html=True)
         time.sleep(delay)
 
-def speech_to_text():
-    """
-    Function to convert speech to text.
-    """
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        st.write("Listening... Please speak now.")
-        audio = r.listen(source)
-    try:
-        text = r.recognize_google(audio)
-        st.success("Speech recognized successfully.")
-        return text
-    except sr.UnknownValueError:
-        st.error("Sorry, I could not understand your speech.")
-        return None
-    except sr.RequestError as e:
-        st.error(f"Sorry, an error occurred: {e}")
-        return None
-
-def display_connect_with_me():
-    """
-    Function to display "Connect with Me" section.
-    """
-    st.sidebar.markdown("""
-        <h3 align="left">Connect with me:</h3>
-        <p align="left">
-        <a href="https://twitter.com/Adityak22723056" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="Twitter" height="30" width="40" /></a>
-        <a href="https://linkedin.com/in/aditya-kumar-tiwari-a14547232" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="LinkedIn" height="30" width="40" /></a>
-        <a href="https://kaggle.com/aditya0kumar0tiwari" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/kaggle.svg" alt="Kaggle" height="30" width="40" /></a>
-        <a href="https://instagram.com/_aadi_anant" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="Instagram" height="30" width="40" /></a>
-        <a href="https://www.leetcode.com/_aadi_anant" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/leet-code.svg" alt="LeetCode" height="30" width="40" /></a>
-        </p>
-    """, unsafe_allow_html=True)
-
-def evaluate_response(response, expected_answer):
-    """
-    Function to evaluate the response based on predefined criteria.
-    Args:
-    - response (str): The response given by the user.
-    - expected_answer (str): The expected correct answer.
-    
-    Returns:
-    - dict: Scores and feedback based on the evaluation criteria.
-    """
-    # Sample scoring criteria
-    clarity = 1 if "clear" in response else 0
-    correctness = 1 if response == expected_answer else 0
-    depth_of_knowledge = 1 if len(response.split()) > 10 else 0
-    relevance = 1 if "relevant" in response else 0
-    
-    total_score = clarity + correctness + depth_of_knowledge + relevance
-    
-    feedback = {
-        "Clarity": clarity,
-        "Correctness": correctness,
-        "Depth of Knowledge": depth_of_knowledge,
-        "Relevance": relevance,
-        "Total Score": total_score,
-        "Feedback": f"Your answer scored {total_score}/4. Clarity: {clarity}, Correctness: {correctness}, Depth of Knowledge: {depth_of_knowledge}, Relevance: {relevance}"
-    }
-    
-    return feedback
 
 def main():
     """
     This function is the main entry point of the application. It sets up the Groq client, the Streamlit interface, and handles the chat interaction.
     """
     st.set_page_config(page_title='FlexiBot', layout='wide')
-
     # Apply custom CSS to improve text layout
     st.markdown(
         """
@@ -121,13 +61,13 @@ def main():
         return
 
     # Display the Groq logo
-    spacer, col = st.columns([3, 1])  
+    spacer, col = st.columns([5, 1])  
     with col:  
         st.image('FlexiBot.jpg')
 
     # The title and greeting message of the Streamlit application
     st.title("Chat with FlexiBot!")
-    st.write("Hi! I'm FlexiBot, your responsive and friendly chatbot. I can help with question and provide information. And I'm super quick! Let's begin our conversation!")
+    st.write("Hi! I'm FlexiBot, your responsive and friendly chatbot. I can help with questions, provide information, or just chat for fun. And I'm super quick! Let's begin our conversation!")
 
     # Add customization options to the sidebar
     st.sidebar.title('Customization')
@@ -139,20 +79,20 @@ def main():
     conversational_memory_length = st.sidebar.slider('Conversational memory length:', 1, 10, value=5)
     
     # Add a slider for typing speed in milliseconds
-    typing_speed = st.sidebar.slider('Typing speed (ms per letter):', 1, 20, value=10)
+    typing_speed = st.sidebar.slider('Typing speed (ms per letter):', 1, 100, value=50)
     typing_delay = typing_speed / 1000  # Convert milliseconds to seconds
 
     memory = ConversationBufferWindowMemory(k=conversational_memory_length, memory_key="chat_history", return_messages=True)
 
-    # Check for chat history in session state
+    # session state variable
     if 'chat_history' not in st.session_state:
         st.session_state.chat_history = []
-    else:
-        for message in st.session_state.chat_history:
-            memory.save_context(
-                {'input': message['human']},
-                {'output': message['AI']}
-            )
+
+    for message in st.session_state.chat_history:
+        memory.save_context(
+            {'input': message['human']},
+            {'output': message['AI']}
+        )
 
     # Initialize Groq Langchain chat object and conversation
     groq_chat = ChatGroq(
@@ -166,67 +106,33 @@ def main():
         # Construct a chat prompt template using various components
         prompt = ChatPromptTemplate.from_messages(
             [
-                SystemMessage(content=system_prompt),
-                MessagesPlaceholder(variable_name="chat_history"),
-                HumanMessagePromptTemplate.from_template("{human_input}"),
+                SystemMessage(
+                    content=system_prompt
+                ),  # This is the persistent system prompt that is always included at the start of the chat.
+
+                MessagesPlaceholder(
+                    variable_name="chat_history"
+                ),  # This placeholder will be replaced by the actual chat history during the conversation. It helps in maintaining context.
+
+                HumanMessagePromptTemplate.from_template(
+                    "{human_input}"
+                ),  # This template is where the user's current input will be injected into the prompt.
             ]
         )
 
         # Create a conversation chain using the LangChain LLM (Language Learning Model)
         conversation = LLMChain(
-            llm=groq_chat,
-            prompt=prompt,
-            verbose=True,
-            memory=memory,
+            llm=groq_chat,  # The Groq LangChain chat object initialized earlier.
+            prompt=prompt,  # The constructed prompt template.
+            verbose=True,   # Enables verbose output, which can be useful for debugging.
+            memory=memory,  # The conversational memory object that stores and manages the conversation history.
         )
-
+        
         # The chatbot's answer is generated by sending the full prompt to the Groq API.
         response = conversation.predict(human_input=user_question)
         message = {'human': user_question, 'AI': response}
         st.session_state.chat_history.append(message)
         display_typing_effect(response, typing_delay)  # Use the typing effect function with delay
-        
-        # Provide evaluation and feedback
-        st.subheader("Evaluation and Feedback")
-        expected_answer = "sample expected answer"  # Placeholder for the expected correct answer
-        feedback = evaluate_response(response, expected_answer)
-        st.write(feedback["Feedback"])
-
-    # Speech-to-text section
-    speech_button = st.button("Use Speech Input")
-    if speech_button:
-        st.write("Please click the button below and start speaking.")
-        user_question = speech_to_text()
-        if user_question:
-            st.write(f"User Question: {user_question}")
-            # Construct a chat prompt template using various components
-            prompt = ChatPromptTemplate.from_messages(
-                [
-                    SystemMessage(content=system_prompt),
-                    MessagesPlaceholder(variable_name="chat_history"),
-                    HumanMessagePromptTemplate.from_template("{human_input}"),
-                ]
-            )
-
-            # Create a conversation chain using the LangChain LLM (Language Learning Model)
-            conversation = LLMChain(
-                llm=groq_chat,
-                prompt=prompt,
-                verbose=True,
-                memory=memory,
-            )
-
-            # The chatbot's answer is generated by sending the full prompt to the Groq API.
-            response = conversation.predict(human_input=user_question)
-            message = {'human': user_question, 'AI': response}
-            st.session_state.chat_history.append(message)
-            display_typing_effect(response, typing_delay)  # Use the typing effect function with delay
-            
-            # Provide evaluation and feedback
-            st.subheader("Evaluation and Feedback")
-            expected_answer = "sample expected answer"  # Placeholder for the expected correct answer
-            feedback = evaluate_response(response, expected_answer)
-            st.write(feedback["Feedback"])
 
     # Display "About the Author" section
     st.sidebar.title("About the Author")
@@ -234,8 +140,22 @@ def main():
     Hi there! I'm Aditya, an aspiring ML engineer with a passion for machine learning, deep learning, and problem-solving. I love working on diverse projects and exploring new technologies. Connect with me to stay updated on my latest projects and endeavors!
     """)
 
+    # Function to display "Connect with Me" section
+    def display_connect_with_me():
+        st.sidebar.markdown("""
+            <h3 align="left">Connect with me:</h3>
+            <p align="left">
+                <a href="https://twitter.com/Adityak22723056" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/twitter.svg" alt="Twitter" height="30" width="40" /></a>
+                <a href="https://linkedin.com/in/aditya-kumar-tiwari-a14547232" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/linked-in-alt.svg" alt="LinkedIn" height="30" width="40" /></a>
+                <a href="https://kaggle.com/aditya0kumar0tiwari" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/kaggle.svg" alt="Kaggle" height="30" width="40" /></a>
+                <a href="https://instagram.com/_aadi_anant" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/instagram.svg" alt="Instagram" height="30" width="40" /></a>
+                <a href="https://www.leetcode.com/_aadi_anant" target="_blank"><img align="center" src="https://raw.githubusercontent.com/rahuldkjain/github-profile-readme-generator/master/src/images/icons/Social/leet-code.svg" alt="LeetCode" height="30" width="40" /></a>
+            </p>
+        """, unsafe_allow_html=True)
+
     # Display "Connect with Me" section
     display_connect_with_me()
+
 
 if __name__ == "__main__":
     main()
